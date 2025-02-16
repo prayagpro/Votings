982b7a5559fef66498fc6c49baee7e1f28021d96
Prayag Adage <113018061+prayagpro@users.noreply.github.com>
2025-02-16 14:55:02 +0530

    Create voting.sol
    Solidity Code

---
+++ voting.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+    constructor() {
+        addCandidate("Prayag");
+        addCandidate("Nupur");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted.");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate.");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
+}
